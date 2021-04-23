d81014d47971a776b1bf5a8435fa0c202211da1f
Luis Solís <37953927+solisgb@users.noreply.github.com>
2021-04-23 14:54:39 +0200

    Add files via upload

--- main02.py
+++ main02.py
@@ -6,28 +6,44 @@ Created on Mon Apr 19 12:03:01 2021
 """
 
 from time import time
-from os.path import join
 import traceback
 
+import pandas as pd
+import numpy as np
+
 import littleLogging as logging
-import asc2xy as axy
+import mdt_asc as mdt
 
 dir_input = r'E:\layers\IGN\MDT'
-filename = ('176.030.667 PNOA_MDT05_ETRS89_HU30_0934_LID.asc',
-            '110.276.488 PNOA_MDT05_ETRS89_HU30_0935_LID.asc',
-            '179.861.936 PNOA_MDT05_ETRS89_HU30_0954_LID.asc',
-            '167.410.938 PNOA_MDT05_ETRS89_HU30_0955_LID.asc',
-            '111.926.843 PNOA_MDT05_ETRS89_HU30_0956_LID.asc',
-            '131.228.095 PNOA_MDT05_ETRS89_HU30_0977_LID.asc')
-filepoints = r'E:\layers\IGN\MDT\PNOA_MDT200_ETRS89_HU30_Murcia.csv'
+filenames = ('PNOA_MDT05_ETRS89_HU30_0934_LID.asc',
+             'PNOA_MDT05_ETRS89_HU30_0935_LID.asc',
+             'PNOA_MDT05_ETRS89_HU30_0954_LID.asc',
+             'PNOA_MDT05_ETRS89_HU30_0955_LID.asc',
+             'PNOA_MDT05_ETRS89_HU30_0956_LID.asc',
+             'PNOA_MDT05_ETRS89_HU30_0977_LID.asc')
+
+filepoints = r'E:\IGME20\20201215_aquifer\_data\JLGA_20210217\CRCC\Pozos_eia2017.csv'
+dtypes = {'gid': 'int32', 'x': 'float32', 'y': 'float32'}
+dtypes2 = (np.int32, np.float32, np.float32)
+
 
 if __name__ == "__main__":
 
     try:
         startTime = time()
 
-        vertex = axy.vertex_get(filename)
-        print(vertex)
+        points = pd.read_csv(filepoints, index_col=0, dtype=dtypes)
+
+        points = np.genfromtxt(filepoints, skip_header=1, delimiter=',',
+                               dtype=dtypes2)
+
+        for fn in filenames:
+            grd = mdt.MDT_asc(dir_input, fn)
+
+
+
+
+
 
 
     except ValueError:
